e33dbb97329c86d2f70aa99333883026c97f6a59
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-12 12:49:08 -0400

    feat: add method to build BST from array of data
    Accompanying additions:
    - Added a pretty print method courtesy of the lesson to print a
      representation of the tree to console.
    - Wrote a simple driver script in the main file.

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,12 +1,34 @@
+require_relative "node"
+
 # A blueprint for instantiating a new BST from an array of data.
 class Tree
   private
 
   attr_accessor :root
+  attr_reader :processed_array
+
+  def initialize(array)
+    @processed_array = array.uniq.sort
+    @root = build_tree(processed_array)
+  end
+
+  def build_tree(array)
+    return if array.empty?
+
+    mid_idx = array.length / 2
+    root = Node.new(array[mid_idx])
+    return root if array.length == 1
+
+    root.left_child = build_tree(array[0..(mid_idx - 1)])
+    root.right_child = build_tree(array[(mid_idx + 1)..])
+    root
+  end
 
   public
 
-  def initialize(array)
-    @root = build_tree(array)
+  def pretty_print(node = root, prefix = "", is_left = true) # rubocop:disable Style/OptionalBooleanParameter
+    pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
   end
 end
