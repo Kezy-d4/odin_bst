831998869562eb32592f39f51485cc60676c4aad
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-14 21:01:18 -0400

    feat(queue): add custom queue class

---
+++ lib/queue.rb
@@ -0,0 +1,25 @@
+# A custom queue implementation to enable level-order traversal of a BST.
+class CustomQueue
+  private
+
+  attr_accessor :data
+
+  def initialize
+    @data = []
+  end
+
+  public
+
+  def enqueue(object)
+    data << object
+    object
+  end
+
+  def dequeue
+    data.shift
+  end
+
+  def read
+    data[0]
+  end
+end
